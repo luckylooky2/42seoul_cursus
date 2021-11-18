88c3909a2bf5ea58a721994cdf842c3b2a2d051e
chanhyle <luckylooky2@naver.com>
2021-11-18 21:24:47 +0900

    ft_atoi fnsh(11.18 #21)

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 12:02:37 by chanhyle          #+#    #+#             */
-/*   Updated: 2021/11/18 19:24:23 by marvin           ###   ########.fr       */
+/*   Updated: 2021/11/18 20:51:29 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 int	ft_atoi(const char *nptr)
 {
+	int			i;
 	int			sign;
 	long long	longlong;
 
-	longlong = 0;
+	i = 0;
 	sign = 1;
+	longlong = 0;
 	while ((9 <= *nptr && *nptr <= 13) || *nptr == 32)
 		nptr++;
 	if (*nptr == '+' || *nptr == '-')
@@ -27,13 +29,13 @@ int	ft_atoi(const char *nptr)
 			sign = -1;
 		nptr++;
 	}
-	while ('0' <= *nptr && *nptr <= '9')
-		longlong = longlong * 10 + (*nptr++ - '0');
+	while ('0' <= nptr[i] && nptr[i] <= '9')
+		longlong = longlong * 10 + (nptr[i++] - '0');
 	if (sign == -1)
 		longlong *= -1;
-	if (sign == 1 && longlong < 0)
+	if (sign == 1 && (longlong < 0 || i > 19))
 		return (-1);
-	else if (sign == -1 && longlong > 0)
+	else if (sign == -1 && (longlong > 0 || i > 19))
 		return (0);
 	else
 		return ((int)longlong);
@@ -46,8 +48,11 @@ int	ft_atoi(const char *nptr)
 int main()
 {
 	// char	n[40] = "-18446744073709551614"; //이 이상은 모두 -1 뱉음
-	char	n[40] = "+\t\v\f\r\n \f1234";
-	// sprintf(n, "%li", LONG_MIN);
+	// char	n[40] = "-18446744073709551614124";
+
+	// char	*n = "-123THERE IS A NYANCAT UNDER YOUR BED";
+	char	n[40] = "-999999999999999999999999999";
+
 	int		i1 = atoi(n);
 	int		i2 = ft_atoi(n);
 
