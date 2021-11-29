a09e5d7cf3e835c733a14bf5921202075c355967
Chanhyung Lee <chanhyle@c6r5s5.42seoul.kr>
2021-11-29 17:46:34 +0900

    need to split function (11.29 #2)

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -18,44 +18,56 @@ char	*get_next_line(int fd)
 	int		i;
 	int		check;
 	char	buf;
-	t_list	**lst;
-
+	t_list	*lst;
+	t_list	*curr;
 
 	i = 0;
-	*lst = NULL;
-	while (buf != '\n' && check != 0)
+	lst = NULL;
+	buf = '\0';
+	while (buf != '\n')
 	{
-		buf = '\0';
 		check = read(fd, &buf, 1);
 		if (check != 0)
-			ft_lstadd_back(lst, ft_lstnew(&buf));
+		{
+			curr = ft_lstnew(&buf);
+			ft_lstadd_back(&lst, curr);
+		}
+		else
+			return (NULL);
 	}
-	while (*lst)
+	curr = lst;
+	new_ary = (char *)malloc(sizeof(char) * (ft_lstsize(curr) + 1));
+	if (new_ary == NULL)
+		return (NULL);
+	curr = lst;
+	while (curr)
 	{
-		printf("%c", *((char *)((*lst)->content)));
-		*lst = (*lst)->next;
+		new_ary[i] = *((char *)((curr)->content));
+		curr = (curr)->next;
+		i++;
 	}
-	// new_ary = (char *)malloc(5);
-	// printf("%p", new_ary);
-	// if (new_ary == NULL)
-	// 	return (NULL);
-	// while (*lst)
-	// {
-	// 	new_ary[i++] = *((char *)((*lst)->content));
-	// 	*lst = (*lst)->next;
-	// }
-	// new_ary[i] = '\0';
-	return (NULL);
+	new_ary[i] = '\0';
+	ft_lstclear(&lst, free);
+	return (new_ary);
 }
 
 int main()
 {
 	int fd;
 	int	i;
+	char	*str;
 
 	i = 0;
+	str = "123";
 	fd = open("a.txt", O_RDONLY);
 
-	get_next_line(fd);
-
+	while (1)
+	{
+		str = get_next_line(fd);
+		if (str == NULL)
+			break ;
+		printf("%s", str);
+		i++;
+	}
+	free(str);
 }
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 20:36:07 by chanhyle          #+#    #+#             */
-/*   Updated: 2021/11/29 01:33:04 by marvin           ###   ########.fr       */
+/*   Updated: 2021/11/29 17:04:35 by chanhyle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,18 @@
 t_list	*ft_lstnew(void *content)
 {
 	t_list	*new_lst;
+	char	*new_str;
 
 	new_lst = (t_list *)malloc(sizeof(t_list));
 	if (new_lst == NULL)
 		return (NULL);
-	new_lst->content = content;
+	new_str = (char *)malloc(sizeof(char));
+	if (new_str == NULL)
+		return (NULL);
+	*new_str = *((char *)(content));
+	new_lst->content = (void *)new_str;
 	new_lst->next = NULL;
 	return (new_lst);
-}
-
-void	ft_lstadd_front(t_list **lst, t_list *new)
-{
-	new->next = *lst;
-	*lst = new;
 }
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
@@ -38,7 +37,10 @@ void	ft_lstadd_back(t_list **lst, t_list *new)
 	i = 0;
 	curr = *lst;
 	if (curr == NULL)
-		ft_lstadd_front(lst, new);
+	{
+		new->next = *lst;
+		*lst = new;
+	}
 	else
 	{
 		while (curr->next)
@@ -58,4 +60,29 @@ int	ft_lstsize(t_list *lst)
 		cnt++;
 	}
 	return (cnt);
+}
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*back;
+
+	if ((*del) == NULL || *lst == NULL)
+		return ;
+	back = (*lst)->next;
+	while (*lst)
+	{
+		ft_lstdelone(*lst, (*del));
+		*lst = back;
+		if (*lst != NULL)
+			back = back->next;
+	}
+	*lst = NULL;
+}
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if ((*del) == NULL || lst == NULL)
+		return ;
+	(*del)(lst->content);
+	free(lst);
 }
