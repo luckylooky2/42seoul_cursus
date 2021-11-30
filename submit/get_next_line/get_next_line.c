458533198cb0e9bd8835c259a87bab6899c4a1d2
Chanhyung Lee <chanhyle@c6r5s5.42seoul.kr>
2021-11-30 18:23:06 +0900

    split functions / need to study fildes buffer (11.30 #4)

--- submit/get_next_line/get_next_line.c
+++ submit/get_next_line/get_next_line.c
@@ -6,37 +6,57 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/28 20:36:00 by chanhyle          #+#    #+#             */
-/*   Updated: 2021/11/29 01:36:57 by marvin           ###   ########.fr       */
+/*   Updated: 2021/11/30 18:21:36 by chanhyle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static	t_list	*ft_make_linked_list(int fd, t_list	*lst, char *buf)
+static t_list	*ft_make_linked_list(t_list *char_lst, char *buf)
 {
-	int		check;
 	char	*new_char;
 	t_list	*new_node;
 
+	new_char = (char *)malloc(sizeof(char));
+	if (new_char == NULL)
+	{
+		ft_lstclear(&char_lst, free);
+		return (NULL);
+	}
+	*new_char = *buf;
+	new_node = ft_lstnew(new_char);
+	if (new_node == NULL)
+	{
+		ft_lstclear(&char_lst, free);
+		return (NULL);
+	}
+	ft_lstadd_back(&char_lst, new_node);
+	return (char_lst);
+}
+
+static	t_list	*ft_read_n_check_file(int fd, t_list *char_lst, char *buf)
+{
+	int		check;
+
+	*buf = '\0';
 	while (*buf != '\n')
 	{
 		check = read(fd, buf, 1);
-		if (check != 0)
+		if (check == 1)
 		{
-			new_char = (char *)malloc(sizeof(char));
-			*new_char = *buf;
-			new_node = ft_lstnew(new_char);
-			if (new_char == NULL || new_node == NULL)
-			{
-				ft_lstclear(&lst, free);
+			char_lst = ft_make_linked_list(char_lst, buf);
+			if (char_lst == NULL)
 				return (NULL);
-			}
-			ft_lstadd_back(&lst, new_node);
 		}
-		else
-			return (lst);
+		else if (check == 0)
+			return (char_lst);
+		else if (check == -1)
+		{
+			ft_lstclear(&char_lst, free);
+			return (NULL);
+		}
 	}
-	return (lst);
+	return (char_lst);
 }
 
 char	*get_next_line(int fd)
@@ -44,16 +64,17 @@ char	*get_next_line(int fd)
 	char	buf[BUFFER_SIZE];
 	char	*new_str;
 	int		i;
-	t_list	*lst;
+	t_list	*char_lst;
 	t_list	*curr;
 
 	i = 0;
-	*buf = '\0';
-	lst = NULL;
-	lst = ft_make_linked_list(fd, lst, buf);
-	if (lst == NULL)
+	char_lst = NULL;
+	if (fd < 0)
 		return (NULL);
-	curr = lst;
+	char_lst = ft_read_n_check_file(fd, char_lst, buf);
+	if (char_lst == NULL)
+		return (NULL);
+	curr = char_lst;
 	new_str = (char *)malloc(sizeof(char) * (ft_lstsize(curr) + 1));
 	if (new_str == NULL)
 		return (NULL);
@@ -63,11 +84,13 @@ char	*get_next_line(int fd)
 		curr = (curr)->next;
 	}
 	new_str[i] = '\0';
-	ft_lstclear(&lst, free);
+	ft_lstclear(&char_lst, free);
 	return (new_str);
 }
 
 /*
+#include <stdio.h>
+#include <fcntl.h>
 int main()
 {
 	int fd;
