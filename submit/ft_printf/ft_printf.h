d3edba1ad7d5f67332871577075a79ba240d823d
Chanhyung Lee <chanhyle@c6r7s1.42seoul.kr>
2021-12-22 13:17:58 +0900

    add prototype in header file (12.22 #2)

--- submit/ft_printf/ft_printf.h
+++ submit/ft_printf/ft_printf.h
@@ -23,8 +23,11 @@ char	*print_address_or_percent(const char *format, va_list ap, int *ret);
 char	*print_signed_int(const char *format, va_list ap, int *ret);
 char	*print_unsigned_int(const char *format, va_list ap, int *ret);
 char	*print_hex(const char *format, va_list ap, int *ret);
+void	*ft_calloc(size_t nmemb, size_t size);
 int		ft_unsigned_number_of_digits(unsigned long long unsigned_n, int flag);
 char	*malloc_u(int n);
 char	*malloc_p(unsigned long long n, int len);
+int		ft_number_of_digits(int n);
+char	*ft_itoa(int n);
 
 #endif
